7e2848c85a78de140674705446cd901237f1248a
Kostas Livieratos <koslibpro@gmail.com>
2016-11-02 15:42:03 +0200

    changed setup.py info

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='django-traffic',
     packages=['django-traffic'],
-    version='0.2',
+    version='1.0.0',
     description='Django middleware that helps visualize your app\'s traffic in Kibana',
     author='Konstantinos Livieratos',
     author_email='livieratos.konstantinos@gmail.com',
     url='https://github.com/koslibpro/django-traffic',
-    download_url='https://github.com/koslibpro/django-traffic/tarball/0.2',
+    download_url='https://github.com/koslibpro/django-traffic/tarball/1.0.0',
     keywords=['django', 'kibana', 'elasticsearch', 'traffic'],
     classifiers=[],
 )
