ffdd61fcb06323abb0565ef99d25f35f966d361d
Kostas Livieratos <koslibpro@gmail.com>
2016-11-22 22:00:36 +0200

    new version change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='livieratos.konstantinos@gmail.com',
     install_requires=required,
     url='https://github.com/koslibpro/django-traffic',
-    download_url='https://github.com/koslibpro/django-traffic/tarball/1.2.0',
+    download_url='https://github.com/koslibpro/django-traffic/tarball/1.2.1',
     keywords=['django', 'kibana', 'elasticsearch', 'traffic'],
     classifiers=[],
 )
